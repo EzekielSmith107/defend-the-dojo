c15760e5e3c865cfb90a734b1fa5c675d9387550
Ezekiel Smith <ezesmith107@gmail.com>
2022-07-26 17:39:48 -0400

    completed up to game over

--- Public/js/classes.js
+++ Public/js/classes.js
@@ -43,6 +43,11 @@ class Enemy {
             y: this.position.y + this.height / 2
         }
         this.radius = 25
+        this.health = 100
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
     }
 
     draw() {
@@ -53,6 +58,13 @@ class Enemy {
         context.beginPath()
         context.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
         context.fill()
+
+        // Health bar
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y - 15, this.width, 10)
+        
+        context.fillStyle = 'green'
+        context.fillRect(this.position.x, this.position.y - 15, this.width * this.health / 100, 10)
     }
 
     update() {
@@ -65,9 +77,14 @@ class Enemy {
         const xDistance = waypoint.x - this.center.x
         const angle = Math.atan2(yDistance, xDistance)
 
+        // Can adjust velocity to speed up enemy
+        const speed = 1.5
+       
+        this.velocity.x = Math.cos(angle) * speed
+        this.velocity.y = Math.sin(angle) * speed
         // Assigning angle to get appropriate velocities. Remember that cos is x and sin is y.
-        this.position.x += Math.cos(angle)
-        this.position.y += Math.sin(angle)
+        this.position.x += this.velocity.x 
+        this.position.y += this.velocity.y 
         // Updating our center as the enemies move
         this.center = {
             x: this.position.x + this.width / 2,
@@ -76,9 +93,12 @@ class Enemy {
 
         // After an enemy reaches a waypoint, move onto the next one
         //? Can Math.round be applied to both ends of the comparision and it still work?
+        //
         if(
-            Math.round(this.center.x) === Math.round(waypoint.x) && 
-            Math.round(this.center.y) === Math.round(waypoint.y) &&
+            Math.abs(Math.round(this.center.x) - Math.round(waypoint.x)) < 
+                Math.abs(this.velocity.x) && 
+            Math.abs(Math.round(this.center.y) - Math.round(waypoint.y)) < 
+                Math.abs(this.velocity.y) &&
             this.waypointIndex < waypoints.length - 1
         ) {
             this.waypointIndex++
@@ -147,6 +167,7 @@ class Building {
         context.fillRect(this.position.x, this.position.y, this.width, this.height)
 
         //! This circle radius is bugged on the last enemy when given a visual. Has no effect on gameplay.
+        //! After updating code, bug is on first enemy while a projectile is not in the air.
         // context.beginPath
         // context.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
         // context.fillStyle = 'rgba(0, 0, 255, 0.2)'
--- Public/js/index.js
+++ Public/js/index.js
@@ -41,27 +41,51 @@ image.src = '../img/map.png'
 
 // Creating new enemies from our class
 const enemySpawn = []
-for(let i = 1; i < 10; i++) {
-    const xOffset = i * 150
-    enemySpawn.push(
-        new Enemy({
-            position: { x: waypoints[0].x - xOffset, y: waypoints[0].y }
-    }))
+
+function spawnEnemies(spawnCount) {
+    for(let i = 1; i < spawnCount + 1; i++) {
+        const xOffset = i * 150
+        enemySpawn.push(
+            new Enemy({
+                position: { x: waypoints[0].x - xOffset, y: waypoints[0].y }
+        }))
+    }
 }
 
 const buildings = []
 //
 let activeTile = undefined
-
+let enemyCount = 3
+let hearts = 10
+spawnEnemies(enemyCount)
 
 // Creating our animation and linking everything together
 function animate() {
-    requestAnimationFrame(animate)
+    const animationId = requestAnimationFrame(animate)
 
     context.drawImage(image, 0, 0)
-    enemySpawn.forEach(enemy => {
+    
+    for(let i = enemySpawn.length - 1; i >= 0; i--) {
+        const enemy = enemySpawn[i]
         enemy.update()
-    }) 
+
+        if(enemy.position.x > canvas.width) {
+            hearts -= 1
+            enemySpawn.splice(i, 1)
+
+            if(hearts === 0) {
+                // Pause the game when hearts reach 0. Works on a window object
+                cancelAnimationFrame(animationId)
+                document.querySelector('#gameOver').style.display = 'flex'
+            }
+        }
+    }
+
+    // tracking total amount of enemies
+    if(enemySpawn.length === 0) {
+        enemyCount += 3
+        spawnEnemies(enemyCount)
+    }
 
     placementTiles.forEach((tile) => {
         tile.update(mouse)
@@ -88,7 +112,20 @@ function animate() {
             const yDistance = projectile.enemy.center.y - projectile.position.y
             const distance = Math.hypot(xDistance, yDistance)
             // When using splice, use a traditional for loop from the back to prevent flicker when using a forEach function
+            
+            // This is when a projectile hits an enemy
             if(distance < projectile.enemy.radius + projectile.radius) {
+               // enemy health and enemy removal
+                projectile.enemy.health -= 20
+                if(projectile.enemy.health <= 0) {
+                    const enemyIndex = enemySpawn.findIndex((enemy) => {
+                        return projectile.enemy === enemy
+                    })
+                    // Added if statement to prevent enemyIndex from being -1 if it doesn't return true. aka enemy died while projectile mid flight
+                    if(enemyIndex > -1) {
+                        enemySpawn.splice(enemyIndex, 1)
+                    }
+                }
                 building.projectiles.splice(i, 1)
             }
         }
